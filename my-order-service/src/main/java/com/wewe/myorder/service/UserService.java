3bbbc5bc54d93b93ad5270940669c93f2fcf92da
jiajia_2311@163.com <jiajia_2311@163.com>
2017-12-29 10:24:04 +0800

    add addUser

--- my-order-service/src/main/java/com/wewe/myorder/service/UserService.java
+++ my-order-service/src/main/java/com/wewe/myorder/service/UserService.java
@@ -1,16 +1,28 @@
 package com.wewe.myorder.service;
 
+import java.util.Date;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import com.google.common.base.Preconditions;
+import com.wewe.myorder.dao.UserDao;
 import com.wewe.myorder.model.User;
 import com.wewe.myorder.request.entity.UserSearchParams;
 
 @Service
 public class UserService {
+  
+  @Resource UserDao userDao;
 
-  public void add(User entity) {
-    // TODO Auto-generated method stub
-    
+  public void add(User user) {
+    user.setId(UUID.randomUUID().toString());
+    user.setCreateTime(new Date());
+    int insert = userDao.insert(user);
+    Preconditions.checkArgument(insert > 0, "添加用户失败");
   }
 
   public void edit(User entity) {
